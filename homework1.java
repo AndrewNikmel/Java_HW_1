ad54b0133550338911d1a082188ffa42426875d1
unknown <Andrew M>
2023-03-30 23:35:44 +0300

    The last task seems to be done

--- homework1.java
+++ homework1.java
@@ -41,32 +41,85 @@
 
 // 2. Task. Вывести все простые числа от 1 до 1000
 
-public class homework1 {
+// public class homework1 {
 
-    public static void main(String [] args){
-        int x, y;
-        for(x = 1; x < 1000; x++){
-            y = 0;
-            for(int i = 1; i <= x; i++){
-                if(x%i == 0){
-                    y++;
-                }
-            }
-            if (y<=2)
-                System.out.print(x + ", ");
-        }
-    }
+//     public static void main(String [] args){
+//         int x, y;
+//         for(x = 1; x < 1000; x++){
+//             y = 0;
+//             for(int i = 1; i <= x; i++){
+//                 if(x%i == 0){
+//                     y++;
+//                 }
+//             }
+//             if (y<=2)
+//                 System.out.print(x + ", ");
+//         }
+//     }
 
-}
+// }
 
 // 3. Task. Реализовать простой калькулятор
+import java.util.Scanner;
 
-// public class homework1 {
-//     public static void main(String[] args) {
-        
-//     }
-    
-// }
+public class homework1 {
+    public static void main(String[] args) {
+        int num1 = getInt();
+        int num2 = getInt();
+        char operation = getOperation();
+        int result = calc(num1,num2,operation);
+        System.out.println("Результат операции: "+result);
+    }
+ 
+    public static int getInt(){
+        System.out.println("Введите число:");
+        int num;
+        if(Scanner.hasNextInt()){
+            num = Scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            Scanner.next();//рекурсия
+            num = getInt();
+        }
+        return num;
+    }
+ 
+    public static char getOperation(){
+        System.out.println("Введите операцию:");
+        char operation;
+        if(Scanner.hasNext()){
+            operation = Scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            Scanner.next();//рекурсия
+            operation = getOperation();
+        }
+        return operation;
+    }
+ 
+    public static int calc(int num1, int num2, char operation){
+        int result;
+        switch (operation){
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1-num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(num1, num2, getOperation());//рекурсия
+        }
+        return result;
+    }
+}
+
 
 
 // 4*. Task +Задано уравнение вида q + w = e, q, w, e >= 0. 
